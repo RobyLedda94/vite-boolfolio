3f2724d41b1541b02138b2b2b6e3080c8a3cab86
RobyLedda94 <robyledda01@yahoo.it>
2024-10-14 16:57:29 +0200

    [UPD] creata la comonente AppProject e definiti i metodi per la ciamata axios

---
+++ src/components/AppProject.vue
@@ -0,0 +1,31 @@
+<script>
+// importo axios
+import axios from 'axios';
+export default {
+    name: 'AppProject',
+    data(){
+        return{
+            projects: []
+        }
+    },
+    created(){
+        this.getProjects();
+    },
+    methods:{
+        getProjects(){
+            axios.get('http://127.0.0.1:8000/api/posts').then((results) => {
+                console.log(results);
+            }); 
+        }
+    }
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
