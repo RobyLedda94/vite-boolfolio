7e32cfb8e43daef49f337269679c6f39c100ec7f
RobyLedda94 <robyledda01@yahoo.it>
2024-10-14 18:17:48 +0200

    [UPD] mostro a video con delle card il contenuto dei post del db, eseguo un v-for per ciclare le card

--- src/components/AppProject.vue
+++ src/components/AppProject.vue
@@ -13,8 +13,8 @@ export default {
     },
     methods:{
         getProjects(){
-            axios.get('http://127.0.0.1:8000/api/posts').then((results) => {
-                console.log(results);
+            axios.get('http://127.0.0.1:8000/api/posts').then((response) => {
+                this.projects = response.data.results;
             }); 
         }
     }
@@ -22,9 +22,22 @@ export default {
 </script>
 <template>
     <div class="container">
-        <div class="row">
+        <div class="row gy-2">
             <div class="col-12">
-                <h1 class="text-center my-3">Prova</h1>
+                <h1 class="text-center my-3">Elenco dei progetti visualizzati a video</h1>
+            </div>
+            <div class="col-12">
+                <div class="row gy-2">
+                    <div class="col-4" v-for="project in projects" :key="project.id">
+                        <div class="card">
+                            <img :src="project.cover_image" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">{{ project.title }}</h5>
+                              <p class="card-text">{{ project.content }}</p>
+                            </div>
+                          </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
