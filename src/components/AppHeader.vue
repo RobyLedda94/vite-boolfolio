e1b3bf02d2b6e41fcb1c66b81ab5d3c0e95d1a4c
RobyLedda94 <robyledda01@yahoo.it>
2024-10-15 16:07:43 +0200

    [UPD] creata la componenete app header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <ul class="list-unstyled d-flex justify-content-start">
+                        <li class="mx-3">
+                            <a href="#">HOME</a>
+                        </li>
+                        <li class="mx-3">
+                            <a href="#">POSTS</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="">
+    
+</style>
